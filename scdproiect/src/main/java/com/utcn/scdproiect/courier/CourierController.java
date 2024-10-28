7f43f18789f5e1db9b2a5a5475ae9c8c1293562b
BogdanBargaoanu <bn19bba@yahoo.com>
2024-10-28 20:51:08 +0200

    Update CourierController.java

--- scdproiect/src/main/java/com/utcn/scdproiect/courier/CourierController.java
+++ scdproiect/src/main/java/com/utcn/scdproiect/courier/CourierController.java
@@ -1,9 +1,10 @@
 package com.utcn.scdproiect.courier;
 
+import com.utcn.scdproiect.pkg.Package;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/couriers")
@@ -11,4 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class CourierController {
     @Autowired
     private CourierService courierService;
+
+    @PostMapping
+    public Courier createCourier(Courier newCourier) {
+        return courierService.createCourier(newCourier);
+    }
+
+    @GetMapping
+    public List<Courier> getAllCouriers() {
+        return courierService.getAllCouriers();
+    }
+
+    @PutMapping("/{id}")
+    public Courier updateCourier(@PathVariable Integer id, @RequestBody Courier updatedCourier) {
+        return courierService.updateCourier(id, updatedCourier);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteCourier(@PathVariable Integer id) {
+        return courierService.deleteCourier(id);
+    }
 }
