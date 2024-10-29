ca6c94923bcf3cb72a03f9f5019f4c19850e9857
BogdanBargaoanu <bn19bba@yahoo.com>
2024-10-29 11:18:29 +0200

    Login page

--- scdproiect/src/main/java/com/utcn/scdproiect/courier/Courier.java
+++ scdproiect/src/main/java/com/utcn/scdproiect/courier/Courier.java
@@ -54,4 +54,14 @@ public class Courier {
     public void setManager(Courier manager) {
         this.manager = manager;
     }
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
