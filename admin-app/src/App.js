9cb8f841cdacbd2dc21ca08c922c91b0448b8932
BogdanBargaoanu <bn19bba@yahoo.com>
2024-11-12 10:47:10 +0200

    Register implementation

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import LoginRegister from './components/Login/LoginRegister';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import { ToastProvider } from './components/Context/Toast/ToastContext';
 
 function App() {
   return (
     <div className="App">
-     <Router>
+      <ToastProvider>
+      <Router>
         <Routes>
           <Route path="/" element={<LoginRegister />} />
           <Route path="/login" element={<LoginRegister />} />
         </Routes>
       </Router>
+      </ToastProvider>
     </div>
   );
 }
--- admin-app/src/components/Login/LoginRegister.jsx
+++ admin-app/src/components/Login/LoginRegister.jsx
@@ -1,61 +1,64 @@
 import React, { useState } from "react"
-//import axios from 'axios'
+import axios from 'axios'
 import './LoginRegister.css'
-import Toast from 'react-bootstrap/Toast'
 import user_icon from '../Assets/person.png'
 import password_icon from '../Assets/password.png'
 import email_icon from '../Assets/email.png'
+import { useToast } from '../Context/Toast/ToastContext'
+import apiKey from '../../API-KEY.json'
 
 const LoginPage = () => {
     const [action, setAction] = useState("Login");
-    const [username, setUsername] = useState("");
+    const [name, setName] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
-    const [showToast, setShowToast] = useState(false);
-    const [toastMessage, setToastMessage] = useState("");
+    const { showToastMessage } = useToast();
 
     const handleLogin = () => {
-       /* axios.post('http://localhost:3000/partners/login', {
-            username: username,
-            password: password
-        })
-            .then(response => {
-                if (response.data.success) {
-                    // The login was successful
-                    localStorage.setItem('user-token', response.data.token);
-                    window.location.href = '/dashboard';
-                }
-            })
-            .catch(error => {
-                setToastMessage('Invalid login: ' + (error.response?.data?.error || 'Unknown error'));
-                setShowToast(true);
-            });
-            */
+        console.log(apiKey);
+        /* axios.post('http://localhost:3000/partners/login', {
+             username: username,
+             password: password
+         })
+             .then(response => {
+                 if (response.data.success) {
+                     // The login was successful
+                     localStorage.setItem('user-token', response.data.token);
+                     window.location.href = '/dashboard';
+                 }
+             })
+             .catch(error => {
+                 showToastMessage('Invalid login: ' + (error.response?.data?.error || 'Unknown error'));
+             });
+             */
     };
 
     const handleSignUp = () => {
-    /*
-        axios.post('http://localhost:3000/partners/addPartner', {
-            username: username,
+        const authHeader = 'Basic ' + btoa(`user:${apiKey.key}`);
+        axios.post('http://localhost:8083/couriers', {
+            name: name,
             email: email,
             password: password,
+            manager_id: null,
             information: `Registration date:${Date.now().toString()}`
-        })
+        },
+            {
+                headers: {
+                    'Authorization': authHeader
+                }
+            })
             .then(response => {
                 if (response.data.success) {
                     // The registration was successful
-                    setUsername("");
+                    setName("");
                     setPassword("");
-                    setToastMessage('Registration successful. Please login.');
-                    setShowToast(true);
+                    showToastMessage('Registration successful. Please login.');
                     setAction("Login");
                 }
             })
             .catch(error => {
-                setToastMessage('Invalid registration: ' + (error.response?.data?.error || 'Unknown error'));
-                setShowToast(true);
+                showToastMessage('Invalid registration: ' + (error.response?.data?.error || 'Unknown error'));
             });
-    */
     };
     return (
         <div className="container-login">
@@ -72,7 +75,7 @@ const LoginPage = () => {
 
                 <div className="input">
                     <img src={user_icon} alt="" />
-                    <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
                 </div>
 
                 <div className="input">
@@ -84,13 +87,6 @@ const LoginPage = () => {
                 <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { action === "Sign Up" ? handleSignUp() : setAction("Sign Up") }}>Sign-up</div>
                 <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => { action === "Login" ? handleLogin() : setAction("Login") }}>Login</div>
             </div>
-            <Toast position="top-end" className="p-3" style={{ position: 'absolute', top: 5, right: 5 }} onClose={() => setShowToast(false)} show={showToast} delay={3000} autohide>
-                <Toast.Header>
-                    <strong className="me-auto">Exchange-Admin</strong>
-                    <small>Now</small>
-                </Toast.Header>
-                <Toast.Body>{toastMessage}</Toast.Body>
-            </Toast>
         </div>)
 }
 
