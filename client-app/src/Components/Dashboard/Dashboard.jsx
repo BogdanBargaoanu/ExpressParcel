234632f99255dc271543c796dedb2ee16079cccf
BogdanBargaoanu <bn19bba@yahoo.com>
2024-12-22 09:55:23 +0200

    Add Dashboard component and update dependencies for React Router and Icons

---
+++ client-app/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import './Dashboard.css';
+import { useNavigate } from 'react-router-dom';
+
+const Dashboard = () => {
+    return (
+        <div className="container-dashboard">
+        </div>
+    );
+};
+
+export default Dashboard;
