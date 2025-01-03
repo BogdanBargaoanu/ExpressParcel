f248a4c7debe3e9375b6c4862f1854b7b731d866
BogdanBargaoanu <bn19bba@yahoo.com>
2025-01-03 18:38:09 +0200

    Add styling and content to Dashboard component; enhance layout and text presentation

--- admin-app/src/components/Dashboard/Dashboard.jsx
+++ admin-app/src/components/Dashboard/Dashboard.jsx
@@ -6,7 +6,14 @@ import './Dashboard.css';
 const Contact = () => {
     return (
         <div className='dashboard-container'>
-
+            <div className="dashboard-content">
+                <h1 className='dashboard-header'>Not your simple delivery solution<br />
+                    <span>ExpressParcel</span>
+                </h1>
+                <p className="dashboard-description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+            </div>
         </div>
     )
 }
