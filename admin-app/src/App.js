6d1531e6469bb6ccdfbcc4c4f0f6ef8403f407bc
BogdanBargaoanu <bn19bba@yahoo.com>
2025-01-03 18:27:03 +0200

    Add Dashboard and PrivateRoute components; implement routing and enhance Navbar styling

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginRegister from './components/Login/LoginRegister';
+import Dashboard from './components/Dashboard/Dashboard';
+import Navbar from './components/Navbar/Navbar';
+import PrivateRoute from './PrivateRoute';
+import logo from './components/Assets/logo.png';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import { ToastProvider } from './components/Context/Toast/ToastContext';
@@ -9,14 +13,32 @@ function App() {
   return (
     <div className="App">
       <ToastProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginRegister />} />
-          <Route path="/login" element={<LoginRegister />} />
-        </Routes>
-      </Router>
+        <Router>
+          <AppContent />
+        </Router>
       </ToastProvider>
     </div>
+  );
+}
+
+function AppContent() {
+  const location = window.location;
+
+  return (
+    <>
+      {location.pathname !== '/login' && (
+        <>
+          <Navbar />
+          <img className="logo" src={logo} alt="" />
+        </>
+      )}
+      <Routes>
+        <Route path="/" element={<PrivateRoute component={Dashboard} />} />
+        <Route path="/login" element={<LoginRegister />} />
+        <Route path="/dashboard" element={<PrivateRoute component={Dashboard} />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </>
   );
 }
 
---
+++ admin-app/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import './Dashboard.css';
+
+const Contact = () => {
+    return (
+        <div className='dashboard-container'>
+
+        </div>
+    )
+}
+
+export default Contact;
